50ac53ab7999121fd4e2e9a2ba4a57c59150f432
swyoo1121 <tonyyoo0304@gmail.com>
2023-03-29 14:34:23 +0900

    230329 update

---
+++ 11279.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N, x;
+    priority_queue<int> pq;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N;
+
+    for (int iter = 0; iter < N; iter++) {
+        cin >> x;
+
+        if (x == 0) {
+            if (pq.empty()) cout << "0\n";
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(x);
+        }
+    }
+
+    return 0;
+}
