c2f8e38345b96a1690cafc3c67bf62af295ce0f2
swyoo1121 <tonyyoo0304@gmail.com>
2023-04-06 13:17:27 +0900

    230405 update

--- 1931.cpp
+++ 1931.cpp
@@ -1,62 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <map>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    int N, max = 0, start, end, s_pos = 0, e_pos;
-    multimap<int, int>mm;
+int main()
+{
+    int N, end, begin;
+    int value[10];
+
+    vector<pair<int, int>> v;
 
     cin >> N;
 
-    for (int iter = 0; iter < N; iter++) {
-        cin >> start >> end;
-        mm.insert(make_pair(start, end));
+    for (int i = 0; i < N; i++)
+    {
+        cin >> begin >> end;
+        v.push_back(make_pair(end, begin));
     }
 
-    for (auto it = mm.begin(); it != mm.end(); it++) {
-        if (it != s_pos) {
-            if (it->second - it->first < )
-        }
-        else {
-            if ()
+    sort(v.begin(), v.end());
+
+    int time = v[0].first;
+    int count = 1;
+    for (int i = 1; i < N; i++)
+    {
+        if (time <= v[i].second)
+        {
+            count++;
+            time = v[i].first;
         }
     }
 
-
+    cout << count;
 }
-
-/*
-int main() {
-    int N, max = 0, start, end, start_pos = 0, end_pos = 0, start_limit, end_limit = 0;
-    map<int, int>m;
-
-    cin >> N;
-
-    for (int iter = 0; iter< N;iter++) {
-        cin >> start >> end;
-
-        if (m.count(start) != 0) {
-            if (m[start] < end;) continue;
-            else {
-                m[start] = end;
-                if (end_limit < end) end_limit = end;
-            }
-        }
-        else {
-            m.insert(make_pair(start, end));
-            if (end_limit < end) end_limit = end;            
-        }
-    }
-    start_limit = m.begin;
-
-    while(true) {
-
-    }
-
-    
-       
-}
-*/
--- 2512.cpp
+++ 2512.cpp
@@ -4,19 +4,47 @@
 
 using namespace std;
 
-int main() {
-    int N, input, M;
+int main()
+{
+    int N, input, M, sum = 0, result;
+    int low, mid, high;
     vector<int> v;
 
     cin >> N;
-    for (int iter = 0; iter < N; iter++) {
+    for (int iter = 0; iter < N; iter++)
+    {
         cin >> input;
         v.push_back(input);
+        sum += input;
     }
     cin >> M;
 
     sort(v.begin(), v.end());
+    low = 0;
+    high = v.at(N - 1);
 
-    
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        sum = 0;
 
+        for (int iter = 0; iter < N; iter++)
+        {
+            if (mid > v.at(iter))
+                sum += v.at(iter);
+            else
+                sum += mid;
+        }
+
+        if (M >= sum)
+        {
+            result = mid;
+            low = mid + 1;
+        }
+        else
+            high = mid - 1;
+    }
+
+    cout << result << endl;
+    return 0;
 }
